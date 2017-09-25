3075227f63f844eb49924e64f730f69301088eed
Alan Jordan <alanpjordan@gmail.com>
2017-09-25 21:13:59 +1300

    First commit

---
+++ client/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <ul className='nav'>
+        <li><a href="#">about</a></li>
+        <li><a href="#">follow me</a></li>
+    </ul>
+  )
+}
+
+export default Header
---
+++ client/containers/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+import Header from '../components/Header'
+
+const App = () => (
+  <Router>
+    <div className='app-container'>
+      <Header />
+    </div>
+  </Router>
+)
+
+export default App
